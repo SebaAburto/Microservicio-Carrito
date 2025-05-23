ecb4c83f6a343358c57eceb85ed058b66b92e34d
SebaAburto <seba.aburto@duocuc.cl>
2025-05-23 17:45:38 -0400

    servicescarrito

---
+++ src/main/java/com/carrito/carrito/Service/ProductoService.java
@@ -0,0 +1,33 @@
+package com.inventario.inventario.service;
+
+import com.inventario.inventario.model.Producto;
+import com.inventario.inventario.repository.ProductoRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class ProductoService {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    public List<Producto> findAll() {
+        return productoRepository.findAll();
+    }
+
+    public Producto findById(Long id) {
+        return productoRepository.findById(id).get();
+    }
+
+    public Producto save(Producto producto) {
+        return productoRepository.save(producto);
+    }
+
+    public void delete(Long id) {
+        productoRepository.deleteById(id);
+    }
+}
