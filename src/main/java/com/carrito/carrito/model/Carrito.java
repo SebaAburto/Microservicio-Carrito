c820855c9652af5a33b5b46df479b9d2ae1e1734
LucianoGodoy22 <luci.godoy@duocuc.cl>
2025-05-29 23:11:06 -0400

    Cambios menores

--- src/main/java/com/carrito/carrito/model/Carrito.java
+++ src/main/java/com/carrito/carrito/model/Carrito.java
@@ -1,15 +1,16 @@
 package com.carrito.carrito.model;
 
 import java.util.Date;
+import java.util.List;
 
-
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -46,7 +47,7 @@ public class Carrito {
     @OneToOne
     private Tipo_estado tipo_estado;
 
-    @ManyToMany
-    private Producto producto;
+    @OneToMany(mappedBy = "carrito", cascade = CascadeType.ALL)
+    private List<Producto> producto;
 }
 
